75157ce9d03559bf4d319894c0cf9fd677f32835
GuineOnBlitz <24HaleEl@berkeleygreenutc.org.uk>
2025-02-25 14:27:25 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,6 +2,19 @@
 function Redirect(Link) {
 	window.location.href = Link
 }
+
+document.onkeypress = function (e) {
+	 e = e || window.event;
+	
+    	if (e.keyCode == 109) {
+		
+		
+		window.location.href = "https://guinea1980-svg.github.io/ZYME/ZYME/gggg/dsipdfiushndjkn.JPG"
+		
+	}
+};
+
+
 function download(file, text) {
 
         //creating an invisible element
