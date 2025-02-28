f10bb2e6d0977b4f9d09a6014a89984a65d465c9
GuineOnBlitz <24HaleEl@berkeleygreenutc.org.uk>
2025-02-28 10:10:32 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,9 +2,14 @@ let Version = "1.05"
 
 console.log("Version: " + Version)
 
+let Cube_X_Old = 0
+let Cube_Y_Old = 0
+let Cube_X = 0
+let Cube_Y = 0
+let Cube_Char = "🟦"
 
 let screen = { 
- X_0_Y_0: "🟩",
+ X_0_Y_0: "🟦",
  X_0_Y_1: "🟥",
  X_0_Y_2: "🟥",
  X_0_Y_3: "🟥",
@@ -16,13 +21,13 @@ let screen = {
 
  X_2_Y_0: "🟥",
  X_2_Y_1: "🟥",
- X_2_Y_2: "🟦",
+ X_2_Y_2: "🟥",
  X_2_Y_3: "🟥",
 
  X_3_Y_0: "🟥",
  X_3_Y_1: "🟥",
  X_3_Y_2: "🟥",
- X_3_Y_3: "🟩",
+ X_3_Y_3: "🟥",
 }
 
 
@@ -30,31 +35,70 @@ let screen = {
 function Update() {
 	
 	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_0_Y_0"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_1_Y_0"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_2_Y_0"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_3_Y_0"]
+	document.getElementsByClassName("X_1_Y_0")[0].innerHTML = screen["X_1_Y_0"]
+	document.getElementsByClassName("X_2_Y_0")[0].innerHTML = screen["X_2_Y_0"]
+	document.getElementsByClassName("X_3_Y_0")[0].innerHTML = screen["X_3_Y_0"]
 
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_0_Y_1"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_1_Y_1"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_2_Y_1"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_3_Y_1"]
+	document.getElementsByClassName("X_0_Y_1")[0].innerHTML = screen["X_0_Y_1"]
+	document.getElementsByClassName("X_1_Y_1")[0].innerHTML = screen["X_1_Y_1"]
+	document.getElementsByClassName("X_2_Y_1")[0].innerHTML = screen["X_2_Y_1"]
+	document.getElementsByClassName("X_3_Y_1")[0].innerHTML = screen["X_3_Y_1"]
 
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_0_Y_2"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_1_Y_2"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_2_Y_2"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_3_Y_2"]
+	document.getElementsByClassName("X_0_Y_2")[0].innerHTML = screen["X_0_Y_2"]
+	document.getElementsByClassName("X_1_Y_2")[0].innerHTML = screen["X_1_Y_2"]
+	document.getElementsByClassName("X_2_Y_2")[0].innerHTML = screen["X_2_Y_2"]
+	document.getElementsByClassName("X_3_Y_2")[0].innerHTML = screen["X_3_Y_2"]
 
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_0_Y_3"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_1_Y_3"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_2_Y_3"]
-	document.getElementsByClassName("X_0_Y_0")[0].innerHTML = screen["X_3_Y_3"]
+	document.getElementsByClassName("X_0_Y_3")[0].innerHTML = screen["X_0_Y_3"]
+	document.getElementsByClassName("X_1_Y_3")[0].innerHTML = screen["X_1_Y_3"]
+	document.getElementsByClassName("X_2_Y_3")[0].innerHTML = screen["X_2_Y_3"]
+	document.getElementsByClassName("X_3_Y_3")[0].innerHTML = screen["X_3_Y_3"]
 	
 	
 	
 }
 
+function MoveCube(Dir) {
 
+	Cube_X_Old = Cube_X
+	Cube_Y_Old = Cube_Y
+	
+	if (Dir == "Up") {
+		
+		Cube_Y += 1
+		
+	}
+	if (Dir == "Down") {
+		
+		Cube_Y -= 1
+		
+	}
+	if (Dir == "Left") {
+		
+		Cube_X += 1
+		
+	}
+	if (Dir == "Right") {
+		
+		Cube_X -= 1
+		
+	}
+	ChangeDisplayCube()
+	
+	
+}
 
+function ChangeDisplayCube() {
+	
+	document.getElementsByClassName(`X_${Cube_X}_Y_${Cube_Y}`)[0].innerHTML = Cube_Char
+	screen[`X_${Cube_X}_Y_${Cube_Y}`] = Cube_Char
+	screen[`X_${Cube_X_Old}_Y_${Cube_Y_Old}`] = "🟥"
+	document.getElementsByClassName(`X_${Cube_X_Old}_Y_${Cube_Y_Old}`)[0].innerHTML = "🟥"
+	
+	
+}
+
+Update()
 
 
 
