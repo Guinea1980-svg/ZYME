1b55a42c8fcb8a867da2a076cfd28c58690b22f4
GuineOnBlitz <24HaleEl@berkeleygreenutc.org.uk>
2025-03-04 15:07:14 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -33,6 +33,7 @@ document.onkeypress = function (e) {
 	}
 	prevkey = e.keyCode
 };
+
 let Cube_X_Old_10 = 0
 let Cube_Y_Old_10 = 0
 let Cube_X_Old_9 = 0
@@ -57,7 +58,10 @@ let Cube_X = 0
 let Cube_Y = 0
 
 let Cube_Length = 1
-let Cube_Char = "🟩"
+let Cube_Char = "🟦"
+let Cube_Body = "🟩"
+
+let Empty = "⬜"
 
 let Coin_X_Old = 0
 let Coin_Y_Old = 0
@@ -66,7 +70,7 @@ let Coin_Y = 5
 let Coin_Char = "🟨"
 
 let screen = { 
- X_0_Y_0: "🟩",
+ X_0_Y_0: Cube_Char,
  X_0_Y_1: "⬜",
  X_0_Y_2: "⬜",
  X_0_Y_3: "⬜",
@@ -90,7 +94,7 @@ let screen = {
  X_2_Y_2: "⬜",
  X_2_Y_3: "⬜",
  X_2_Y_4: "⬜",
- X_2_Y_5: "🟨",
+ X_2_Y_5: Coin_Char,
  X_2_Y_6: "⬜",
  X_2_Y_7: "⬜",
 
@@ -218,14 +222,71 @@ function Update() {
 	
 	
 	
-}
+};
+
 
 function RandomizeCoin() {
+	
 	Coin_X_Old = Coin_X
 	Coin_Y_Old = Coin_Y
 	
-	Coin_X = Math.floor((Math.random() * 7) + 1);
-	Coin_Y = Math.floor((Math.random() * 7) + 1);
+
+
+	let Valid = false
+	
+	while (Valid == false) {
+  		Coin_X = Math.floor((Math.random() * 7) + 1);
+		Coin_Y = Math.floor((Math.random() * 7) + 1);
+
+		if (screen[`X_${Coin_X}_Y_${Coin_Y}`] != Cube_Body) {
+			
+			screen[`X_${Coin_X}_Y_${Coin_Y}`] = Coin_Char
+			
+			screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`] = Cube_Char
+			
+			document.getElementsByClassName(`X_${Coin_X}_Y_${Coin_Y}`)[0].innerHTML = screen[`X_${Coin_X}_Y_${Coin_Y}`]
+			
+			document.getElementsByClassName(`X_${Coin_X_Old}_Y_${Coin_Y_Old}`)[0].innerHTML = screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`]
+
+			Valid = true
+
+			return 0
+			
+			
+		}
+		if (screen[`X_${Coin_X}_Y_${Coin_Y}`] != Cube_Char) {
+			
+			screen[`X_${Coin_X}_Y_${Coin_Y}`] = Coin_Char
+			
+			screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`] = Cube_Char
+			
+			document.getElementsByClassName(`X_${Coin_X}_Y_${Coin_Y}`)[0].innerHTML = screen[`X_${Coin_X}_Y_${Coin_Y}`]
+			
+			document.getElementsByClassName(`X_${Coin_X_Old}_Y_${Coin_Y_Old}`)[0].innerHTML = screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`]
+
+			Valid = true
+			
+			return 0
+			
+		}
+		if (Coin_X_Old != Coin_X ) {
+			if (Coin_Y_Old != Coin_Y ) {
+			
+				screen[`X_${Coin_X}_Y_${Coin_Y}`] = Coin_Char
+			
+				screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`] = Cube_Char
+			
+				document.getElementsByClassName(`X_${Coin_X}_Y_${Coin_Y}`)[0].innerHTML = screen[`X_${Coin_X}_Y_${Coin_Y}`]
+			
+				document.getElementsByClassName(`X_${Coin_X_Old}_Y_${Coin_Y_Old}`)[0].innerHTML = screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`]
+
+				Valid = true
+
+				return 0
+			
+		}
+		
+	}	
 
 	screen[`X_${Coin_X}_Y_${Coin_Y}`] = Coin_Char
 	screen[`X_${Coin_X_Old}_Y_${Coin_Y_Old}`] = Cube_Char
@@ -234,93 +295,80 @@ function RandomizeCoin() {
 
 	Cube_Length += 1
 	
-}
+};
+
+function LogCubeHistory() {
+	
+	Cube_X_Old_10 = Cube_X_Old_9
+	Cube_Y_Old_10 = Cube_Y_Old_9
+	Cube_X_Old_9 = Cube_X_Old_8
+	Cube_Y_Old_9 = Cube_Y_Old_8
+	Cube_X_Old_8 = Cube_X_Old_7
+	Cube_Y_Old_8 = Cube_Y_Old_7
+	Cube_X_Old_7 = Cube_X_Old_6
+	Cube_Y_Old_7 = Cube_Y_Old_6
+	Cube_X_Old_6 = Cube_X_Old_5
+	Cube_Y_Old_6 = Cube_Y_Old_5
+	Cube_X_Old_5 = Cube_X_Old_4
+	Cube_Y_Old_5 = Cube_Y_Old_4
+	Cube_X_Old_4 = Cube_X_Old_3
+	Cube_Y_Old_4 = Cube_Y_Old_3
+	Cube_X_Old_3 = Cube_X_Old_2
+	Cube_Y_Old_3 = Cube_Y_Old_2
+	Cube_X_Old_2 = Cube_X_Old
+	Cube_Y_Old_2 = Cube_Y_Old
+	Cube_X_Old = Cube_X
+	Cube_Y_Old = Cube_Y
+	
+};
+
+function CheckForCoin() {
+	if (Coin_X == Cube_X) {
+		if (Coin_Y == Cube_Y) {		
+			RandomizeCoin()
+		}
+	}
+};
 
 function MoveCube(Dir) {
 	
 	if (Dir == "Up") {
 
 		if (Cube_Y != 7) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
+			
 			Cube_Y += 1
+			
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		if (Cube_Y == 7) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			
+			LogCubeHistory()
+			
 			Cube_Y = 0
+			
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			
+			CheckForCoin()
 			return 0
 		}
 	}
 	if (Dir == "Down") {
 		
 		if (Cube_Y != 0) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_Y -= 1
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		if (Cube_Y == 0) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_Y = 7
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		
@@ -328,91 +376,39 @@ function MoveCube(Dir) {
 	if (Dir == "Left") {
 
 		if (Cube_X != 7) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_X += 1
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		if (Cube_X == 7) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_X = 0
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		
 	}
 	if (Dir == "Right") {
 		if (Cube_X != 0) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_X -= 1
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		if (Cube_X == 0) {
-			Cube_X_Old_5 = Cube_X_Old_4
-			Cube_Y_Old_5 = Cube_Y_Old_4
-			Cube_X_Old_4 = Cube_X_Old_3
-			Cube_Y_Old_4 = Cube_Y_Old_3
-			Cube_X_Old_3 = Cube_X_Old_2
-			Cube_Y_Old_3 = Cube_Y_Old_2
-			Cube_X_Old_2 = Cube_X_Old
-			Cube_Y_Old_2 = Cube_Y_Old
-			Cube_X_Old = Cube_X
-			Cube_Y_Old = Cube_Y
+			LogCubeHistory()
 			Cube_X = 7
 			ChangeDisplayCube()
-			if (Coin_X == Cube_X) {
-				if (Coin_Y == Cube_Y) {
-					RandomizeCoin()
-				}
-			}
+			CheckForCoin()
 			return 0
 		}
 		
 	}
-}
+};
 
 function ChangeDisplayCube() {
 
@@ -497,7 +493,7 @@ function ChangeDisplayCube() {
 	Update()
 	
 	
-}
+};
 
 
 
@@ -512,12 +508,12 @@ function ChangeDisplayCube() {
 
 function Redirect(Link) {
 	window.location.href = Link
-}
+};
 
 function NewTabLink(url) {
 
 	window.open(url, '_blank').focus();
-}
+};
 
 
 
@@ -533,4 +529,4 @@ function download(file, text) {
         element.click();
 
 	document.body.removeChild(element);
-}
+};
