1e8a54ff12495de70f60fba31406c198ea0b1f8a
GuineOnBlitz <24HaleEl@berkeleygreenutc.org.uk>
2025-06-11 11:52:50 +0100

    f

--- Script.js
+++ Script.js
@@ -31,7 +31,7 @@ function Update_Site(data) {
 
           console.log(data.util[index + 1].link)
 
-          document.getElementsByClassName("card")[index].children[number].onclick = `NewTabLink("${data.util[index + 1].link}")`
+          document.getElementsByClassName("card")[index].children[number].onclick = "NewTabLink(`${data.util[index + 1].link}`)"
 
         }
       }
@@ -169,6 +169,8 @@ function Redirect(Link) {
 };
 
 function NewTabLink(url) {
+
+  console.log(url)
 
   window.open(url, '_blank').focus();
 };
